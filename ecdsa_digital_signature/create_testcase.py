a5ee1f7bac0d8e78b48b1671e64042d95693248a
Tim <timkin99@mail.ru>
2022-10-28 16:26:12 +0300

    Реализация проверки подписи на основе другой библиотеки(cryptography)

--- ecdsa_digital_signature/create_testcase.py
+++ ecdsa_digital_signature/create_testcase.py
@@ -1,41 +1,60 @@
-from ecdsa import SigningKey, VerifyingKey, BadSignatureError
-from ecdsa.curves import NIST256p
-from hashlib import sha256
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
+from cryptography.exceptions import InvalidSignature
 
 
 def create_keys():
-    sk = SigningKey.generate(curve=NIST256p, hashfunc=sha256)
-    vk = sk.verifying_key
-    with open("private.pem", "wb") as f:
-        f.write(sk.to_pem())
-    with open("public.pem", "wb") as f:
-        f.write(vk.to_pem())
+    private_key = ec.generate_private_key(ec.SECP256R1())
+    public_key = private_key.public_key()
+
+    serialized_private = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.BestAvailableEncryption(b"key")
+    )
+    with open("private.pem", "wb+") as file:
+        file.write(serialized_private)
+
+    serialized_public = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    with open("public.pem", "wb+") as file:
+        file.write(serialized_public)
+    return private_key
 
 
-def create_signature():
-    with open("private.pem") as f:
-        sk = SigningKey.from_pem(f.read())
-    with open("secret.txt", "rb") as f:
-        message = f.read()
-    sig = sk.sign(message)
-    with open("secret.sign", "wb") as f:
-        f.write(sig)
+def create_signature(private_key):
+    with open("secret.txt", 'rb') as file:
+        data = file.read()
+
+    signature = private_key.sign(data, ec.ECDSA(hashes.SHA256()))
+
+    with open("secret.sign", 'wb') as file:
+        file.write(signature)
 
 
 def check_signature():
-    vk = VerifyingKey.from_pem(open("public.pem").read())
-    with open("secret.txt", "rb") as f:
-        message = f.read()
-    with open("secret.sign", "rb") as f:
-        sig = f.read()
+    with open("secret.txt", 'rb') as file:
+        message = file.read()
+
+    with open("secret.sign", 'rb') as file:
+        signature = file.read()
+
+    with open("public.pem", 'rb') as file:
+        serialized_public = file.read()
+    loaded_public_key = serialization.load_pem_public_key(serialized_public)
     try:
-        vk.verify(sig, message)
-        print("good signature")
-    except BadSignatureError:
-        print("BAD SIGNATURE")
+        loaded_public_key.verify(signature=signature,
+                                 data=message,
+                                 signature_algorithm=ec.ECDSA(hashes.SHA256()))
+        print(True)
+    except InvalidSignature:
+        print(False)
 
 
 if __name__ == '__main__':
-    create_keys()
-    create_signature()
+    private_key = create_keys()
+    create_signature(private_key)
     check_signature()
--- ecdsa_digital_signature/ecdsa_digital_signature.py
+++ ecdsa_digital_signature/ecdsa_digital_signature.py
@@ -1,19 +1,27 @@
-from ecdsa import VerifyingKey, BadSignatureError
-from hashlib import sha256
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
+from cryptography.exceptions import InvalidSignature
 import click
 
 
-def check_signature(filename: str, signature: str, key: str) -> str:
-    vk = VerifyingKey.from_pem(open(key).read(), hashfunc=sha256)
-    with open(filename, "rb") as f:
-        message = f.read()
-    with open(signature, "rb") as f:
-        sig = f.read()
+def check_signature(filename: str, signature: str, key: str) -> bool:
+    with open(filename, 'rb') as file:
+        message = file.read()
+
+    with open(signature, 'rb') as file:
+        sig = file.read()
+
+    with open(key, 'rb') as file:
+        serialized_public = file.read()
+    loaded_public_key = serialization.load_pem_public_key(serialized_public)
     try:
-        vk.verify(sig, message)
-        return "VALID"
-    except BadSignatureError:
-        return "INVALID"
+        loaded_public_key.verify(signature=sig,
+                                 data=message,
+                                 signature_algorithm=ec.ECDSA(hashes.SHA256()))
+        return True
+    except InvalidSignature:
+        return False
 
 
 @click.command()
@@ -22,8 +30,9 @@ def check_signature(filename: str, signature: str, key: str) -> str:
 @click.option('--k', prompt='Key', help="--k <KEY> -- filename of a public key")
 def script(f, s, k):
     answer = check_signature(f, s, k)
-    click.echo(answer)
+    click.echo("VALID" if answer else "INVALID")
 
 
 if __name__ == '__main__':
+    # start check script
     script()
