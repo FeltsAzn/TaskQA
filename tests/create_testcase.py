ec4444be2080c3978b93e47a909f469dcf29807f
Tim <timkin99@mail.ru>
2022-10-29 14:01:55 +0300

    Тесты для скрипта

--- tests/create_testcase.py
+++ tests/create_testcase.py
@@ -5,42 +5,42 @@ from dotenv import load_dotenv
 import os
 
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env_test')
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env_test')  # Finding the path of the .env_test file
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-password = bytes(os.getenv("TEST_PASSWORD_FOR_PRIVATE_KEY"), "utf-8")
+password = bytes(os.getenv("TEST_PASSWORD_FOR_PRIVATE_KEY"), "utf-8")  # Load test secret password on .env_test
 
 
-def create_keys():
+def create_test_keys() -> None:
+    """Function for create test public and private key"""
     private_key = ec.generate_private_key(ec.SECP256R1())
-    public_key = private_key.public_key()
-
     serialized_private = private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
         encryption_algorithm=serialization.BestAvailableEncryption(password)
     )
-    with open("test_keys/private.pem", "wb+") as file:
+    with open("test_keys/test_private.pem", "wb+") as file:
         file.write(serialized_private)
 
+    public_key = private_key.public_key()
     serialized_public = public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     )
-    with open("test_keys/public.pem", "wb+") as file:
+    with open("test_keys/test_public.pem", "wb+") as file:
         file.write(serialized_public)
-    return private_key
 
 
-def create_signature():
+def create_test_signature() -> None:
+    """Function for create test digital signature with private key"""
     with open("test_keys/test_secret.txt", 'rb') as file:
         data = file.read()
-    with open("test_keys/private.pem", 'rb') as private_key:
+
+    with open("test_keys/test_private.pem", 'rb') as private_key:
         serialized_private = private_key.read()
     loaded_private_key = serialization.load_pem_private_key(serialized_private,
                                                             password=password)
 
     signature = loaded_private_key.sign(data, ec.ECDSA(hashes.SHA256()))
-
     with open("test_keys/test_secret.sign", 'wb') as file:
         file.write(signature)
--- tests/test_ecdsa_verify.py
+++ tests/test_ecdsa_verify.py
@@ -1,31 +1,50 @@
 from signature_script.ecdsa_digital_signature import check_signature
-from create_testcase import create_keys, create_signature
-import os
-from dotenv import load_dotenv
+from create_testcase import create_test_keys, create_test_signature
+import pytest
 
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env_test')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
-print(dotenv_path)
-password = bytes(os.getenv("TEST_PASSWORD_FOR_PRIVATE_KEY"), "utf-8")
+create_test_keys()
+create_test_signature()
 
 
-def test_check_signature():
+test_data = [("test_keys/test_secret.txt",
+              "test_keys/test_secret.sign",
+              "test_keys/test_public.pem", True),
+
+             ("test_keys/test_secret.txt",
+              "test_keys/false_test_secret.sign",
+              "test_keys/test_public.pem", False),
+
+             ("test_keys/test_secret.txt",
+              "test_keys/test_secret.sign",
+              "test_keys/false_test_public.pem", False)]
+
+test_data_errors = [
+             ("test_keys/test_secret_not_found.txt",
+              "test_keys/test_secret.sign",
+              "test_keys/test_public.pem",
+              FileNotFoundError),
+
+             ("test_keys/test_secret_not_found.txt",
+              "test_keys/test_secret_not_found.sign",
+              "test_keys/test_public.pem",
+              FileNotFoundError),
+
+             ("test_keys/test_secret.txt",
+              "test_keys/test_secret.sign",
+              "test_keys/test_public_not_found.pem",
+              FileNotFoundError)
+             ]
+
+
+@pytest.mark.parametrize("filename, signature, key, value", test_data)
+def test_check_signature(filename: str, signature: str, key: str, value: bool):
     """Test for signature script checker"""
-    create_keys()
-    create_signature()
-    filename_path = "test_keys/test_secret.txt"
-    signature_path = "test_keys/test_secret.sign"
-    pub_key_path = "test_keys/public.pem"
-    assert check_signature(filename=filename_path, signature=signature_path, key=pub_key_path) is True
+    assert check_signature(filename=filename, signature=signature, key=key) is value
 
 
-def test_check_signature_false():
-    """Test for right error on signature script checker"""
-    create_keys()
-    create_signature()
-    filename_path = "test_keys/test_secret.txt"
-    signature_path = "test_keys/false_test_secret.sign"
-    pub_key_path = "test_keys/public.pem"
-    assert check_signature(filename=filename_path, signature=signature_path, key=pub_key_path) is False
+@pytest.mark.parametrize("filename, signature, key, excepted_ex", test_data_errors)
+def test_check_signature_with_error(filename: str, signature: str, key: str, excepted_ex):
+    """Test for signature script checker with errors"""
+    with pytest.raises(excepted_ex):
+        check_signature(filename=filename, signature=signature, key=key)
